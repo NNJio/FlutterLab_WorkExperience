51984d355cdc3b97ee0bbebad298a1da50bf2a1f
NNJio <phatsakorn.newz@gmail.com>
2022-08-21 17:51:24 +0700

    TestClone

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
 
       ),
       debugShowCheckedModeBanner: false,
-      title: 'WorkExperience',
+      title: 'TestEiEi',
       home: const OnboardingPage() 
     );
   }
