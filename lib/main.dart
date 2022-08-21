b4f29c117691d8da120ca3e9bd09a4860d484a18
NNJio <phatsakorn.newz@gmail.com>
2022-08-21 17:54:19 +0700

    Delete things that are not in use

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
 
       ),
       debugShowCheckedModeBanner: false,
-      title: 'TestEiEi',
+      title: 'WorkExperience',
       home: const OnboardingPage() 
     );
   }
